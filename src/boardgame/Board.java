3b62d0fef90f4b2bf6c9b83468427b6709d0e70f
caiogimenes <ciaonemegi@gmail.com>
2026-06-01 20:26:21 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -34,4 +34,8 @@ public class Board {
     public Piece piece(Position position){
         return pieces[position.getRow()][position.getCol()];
     }
+
+    public void placePiece(Piece piece, Position position){
+        this.pieces[position.getRow()][position.getCol()] = piece;
+    }
 }
