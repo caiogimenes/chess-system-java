da44dac1842af398cc8aa6b877118eb23104102d
= <=>
2026-07-17 14:42:46 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,14 +23,18 @@ public class ChessMatch {
         return mat;
     }
 
+    public void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     public void initialSetup() {
         // Rooks
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0,0));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(0,7));
-        board.placePiece(new Rook(board, Color.BLACK), new Position(7,0));
-        board.placePiece(new Rook(board, Color.BLACK), new Position(7,7));
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('h', 8, new Rook(board, Color.WHITE));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('h', 1, new Rook(board, Color.BLACK));
         // Kings
-        board.placePiece(new King(board, Color.WHITE), new Position(0,4));
-        board.placePiece(new King(board, Color.BLACK), new Position(7,4));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
     }
 }
